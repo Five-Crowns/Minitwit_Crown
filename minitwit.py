e3a2539214e98034d1faa37aa6284098fd4a9a0c
Noah <gafa@itu.dk>
2025-01-31 16:23:23 +0100

    Fixed not accounting for utf-8 encodings and a relocated database.
    Since the database was no longer in /tmp/ but alongside our files, the database location string has been changed accordingly.
    
    Also, to account for the incorrect utf-8 encodings, a simple '.decode(utf-8)' has been slapped on, everywhere that needed it. It's not a pretty solution, I hate it in fact, but it works.

--- minitwit.py
+++ minitwit.py
@@ -20,7 +20,7 @@ from flask import Flask, request, session, url_for, redirect, \
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # configuration
-DATABASE = '/tmp/minitwit.db'
+DATABASE = 'minitwit.db'
 PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -38,7 +38,7 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+            db.cursor().executescript(f.read().decode('utf-8'))
         db.commit()
 
 
