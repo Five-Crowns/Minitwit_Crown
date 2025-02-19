9087461abbfd401681c189caa6b0be904e352ec5
gafa <gafa@itu.dk>
2025-02-19 21:35:36 +0100

    Added /api/ endpoints to all normal endpoints.
    Also turned the therefore duplicated code into reusable methods.

--- minitwit.rb
+++ minitwit.rb
@@ -44,7 +44,7 @@ def init_db
 end
 
 # Ensure the database is initialized before the app starts
-init_db if !File.exist?(DATABASE)
+init_db unless File.exist?(DATABASE)
 
 # Query the database
 def query_db(query, *args)
@@ -70,6 +70,134 @@ def gravatar_url(email, size = 80)
   "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email.strip.downcase)}?d=identicon&s=#{size}"
 end
 
+# Formatter for API responses
+def api_response(error, success_message)
+  if error.nil?
+    { status: 'success', message: success_message }.to_json
+  else
+    { status: 'error', message: error }.to_json
+  end
+end
+
+# General endpoint methods
+def public_timeline
+  @messages = query_db("
+    SELECT message.*, user.* FROM message, user
+    WHERE message.flagged = 0 AND message.author_id = user.user_id
+    ORDER BY message.pub_date DESC LIMIT ?", PER_PAGE
+  )
+  nil
+end
+
+def user_timeline(username)
+  @profile_user = query_db('SELECT * FROM user WHERE username = ?', username).first
+  halt 404 if @profile_user.nil?
+
+  @followed = false
+  if @user
+    @followed = !query_db(
+      'SELECT 1 FROM follower WHERE follower.who_id = ? AND follower.whom_id = ?',
+      [session[:user_id], @profile_user['user_id']]
+    ).empty?
+  end
+
+  @messages = query_db('''
+    SELECT message.*, user.* FROM message, user WHERE
+    user.user_id = message.author_id AND user.user_id = ?
+    ORDER BY message.pub_date DESC LIMIT ?''',
+                       [@profile_user['user_id'], PER_PAGE])
+  nil
+end
+
+def login_user(username, password)
+  if !session[:user_id].nil?
+    'You are already logged in'
+  elsif username.to_s.empty?
+    'You have to enter a username'
+  elsif password.to_s.empty?
+    'You have to enter a password'
+  else
+    @username = username
+    user = query_db('SELECT * FROM user WHERE username = ?', username).first
+    if user.nil? || !(BCrypt::Password.new(user["pw_hash"]) == password)
+      'Invalid username or password'
+    else
+      session[:user_id] = user["user_id"]
+      nil
+    end
+  end
+end
+
+def register_user(username, email, password, password2)
+  @username = username
+  @email = email
+
+  if username.to_s.empty?
+    'You have to enter a username'
+  elsif email.to_s.empty? || !email.include?('@')
+    'You have to enter a valid email address'
+  elsif password.to_s.empty?
+    'You have to enter a password'
+  elsif password != password2
+    'The two passwords do not match'
+  elsif !query_db('SELECT user_id FROM user WHERE username = ?', username).empty?
+    'The username is already taken'
+  else
+    pw_hash = BCrypt::Password.create(password)
+    query_db(
+      'INSERT INTO user (username, email, pw_hash) VALUES (?, ?, ?)',
+      [username, email, pw_hash]
+    )
+    nil
+  end
+end
+
+def logout
+  if session[:user_id].nil?
+    'You are not logged in'
+  else
+    session[:user_id] = nil
+    nil
+  end
+end
+
+def add_message(text)
+  halt 401 unless session[:user_id]
+  if text.nil? || text.empty?
+    return "You can't post an empty message."
+  end
+
+  query_db(
+    'INSERT INTO message (author_id, text, pub_date, flagged) VALUES (?, ?, ?, 0)',
+    [session[:user_id], text, Time.now.to_i]
+  )
+  nil
+end
+
+def follow(username)
+  halt 401 unless @user
+  whom_id = get_user_id(username)
+  halt 404 if whom_id.nil?
+
+  query_db(
+    'INSERT INTO follower (who_id, whom_id) VALUES (?, ?)',
+    [session[:user_id], whom_id]
+  )
+  nil
+end
+
+def unfollow(username)
+  halt 401 unless @user
+  whom_id = get_user_id(username)
+  halt 404 if whom_id.nil?
+
+  query_db(
+    'DELETE FROM follower WHERE who_id = ? AND whom_id = ?',
+    [session[:user_id], whom_id]
+  )
+  nil
+end
+
 # Sinatra routes
 before do
   @db = connect_db
@@ -80,6 +208,7 @@ after do
   @db.close if @db
 end
 
+# HTML Endpoints
 get '/' do
   if @user.nil?
     redirect to('/public')
@@ -97,10 +226,7 @@ get '/' do
 end
 
 get '/public' do
-  @messages = query_db("
-    SELECT message.*, user.* FROM message, user
-    WHERE message.flagged = 0 AND message.author_id = user.user_id
-    ORDER BY message.pub_date DESC LIMIT ?", PER_PAGE)
+  public_timeline
   erb :timeline
 end
 
@@ -111,14 +237,11 @@ get '/login' do
 end
 
 post '/login' do
-  user = query_db('SELECT * FROM user WHERE username = ?', params['username']).first
-  if user.nil? || !(BCrypt::Password.new(user["pw_hash"]) == params['password'])
-    @error = 'Invalid username or password'
-    @username = params['username']
-    erb :login, layout: :layout
-  else
-    session[:user_id] = user["user_id"]
+  @error = login_user(params['username'], params['password'])
+  if @error.nil?
     redirect to('/')
+  else
+    erb :login, layout: :layout
   end
 end
 
@@ -130,81 +253,82 @@ get '/register' do
 end
 
 post '/register' do
-  @error = nil
-  @username = params['username']
-  @email = params['email']
-
-  if params['username'].to_s.empty?
-    @error = 'You have to enter a username'
-  elsif params['email'].to_s.empty? || !params['email'].include?('@')
-    @error = 'You have to enter a valid email address'
-  elsif params['password'].to_s.empty?
-    @error = 'You have to enter a password'
-  elsif params['password'] != params['password2']
-    @error = 'The two passwords do not match'
-  elsif !query_db('SELECT user_id FROM user WHERE username = ?', params['username']).empty?
-    @error = 'The username is already taken'
-  else
-    pw_hash = BCrypt::Password.create(params['password'])
-    query_db('INSERT INTO user (username, email, pw_hash) VALUES (?, ?, ?)',
-              [params['username'], params['email'], pw_hash])
+  @error = register_user(params['username'], params['email'], params['password'], params['password2'])
+  if @error.nil?
     redirect to('/login')
+  else
+    erb :register, layout: :layout
   end
-  
-  erb :register, layout: :layout
 end
 
 get '/logout' do
-  session[:user_id] = nil
+  logout
   redirect to('/public')
 end
 
-get '/:username/follow' do
-  halt 401 unless @user
-  whom_id = get_user_id(params[:username])
-  halt 404 if whom_id.nil?
+post '/add_message' do
+  @error = add_message(params['text'])
+  redirect to('/')
+end
 
-  query_db('INSERT INTO follower (who_id, whom_id) VALUES (?, ?)', 
-           [session[:user_id], whom_id])
+get '/:username' do
+  user_timeline(params[:username])
+  erb :timeline
+end
+
+get '/:username/follow' do
+  @error = follow(params[:username])
   redirect to("/#{params[:username]}")
 end
 
 get '/:username/unfollow' do
-  halt 401 unless @user
-  whom_id = get_user_id(params[:username])
-  halt 404 if whom_id.nil?
-
-  query_db('DELETE FROM follower WHERE who_id = ? AND whom_id = ?', 
-           [session[:user_id], whom_id])
+  @error = unfollow(params[:username])
   redirect to("/#{params[:username]}")
 end
 
-post '/add_message' do
-  halt 401 unless session[:user_id]
-  if params['text'] && !params['text'].empty?
-    query_db('INSERT INTO message (author_id, text, pub_date, flagged) VALUES (?, ?, ?, 0)',
-             [session[:user_id], params['text'], Time.now.to_i])
-    redirect to('/')
-  end
+# JSON endpoints for the API
+before do
+  content_type :json if request.path.start_with?('/api/')
 end
 
-get '/:username' do
-  @profile_user = query_db('SELECT * FROM user WHERE username = ?', params[:username]).first
-  halt 404 if @profile_user.nil?
+get '/api/public' do
+  @error = public_timeline
+  api_response(@error, @messages.map { |msg| {user: msg['username'], text: msg['text'], timestamp: format_datetime(msg['pub_date'])} })
+end
 
-  @followed = false
-  if @user
-    @followed = !query_db('SELECT 1 FROM follower WHERE follower.who_id = ? AND follower.whom_id = ?',
-                          [session[:user_id], @profile_user['user_id']]).empty?
-  end
+post '/api/login' do
+  @error = login_user(params['username'], params['password'])
+  api_response(@error, "Logged in as #{@username}")
+end
 
-  @messages = query_db('''
-    SELECT message.*, user.* FROM message, user WHERE
-    user.user_id = message.author_id AND user.user_id = ?
-    ORDER BY message.pub_date DESC LIMIT ?''',
-    [@profile_user['user_id'], PER_PAGE])
+post '/api/register' do
+  @error = register_user(params['username'], params['email'], params['password'], params['password2'])
+  api_response(@error, "Registered as #{@username}")
+end
 
-  erb :timeline
+post '/api/logout' do
+  @error = logout
+  api_response(@error, 'Logged out')
+end
+
+post 'api/add_message' do
+  @error = add_message(params['text'])
+  api_response(@error, 'Message added')
+end
+
+get '/api/:username' do
+  @error = user_timeline(params[:username])
+  api_response(@error, @messages.map { |msg| {text: msg['text'], timestamp: format_datetime(msg['pub_date'])} })
+end
+
+post '/api/:username/follow' do
+  @error = follow(params[:username])
+  api_response(@error, "Now following #{params[:username]}")
+end
+
+post '/api/:username/unfollow' do
+  @error = unfollow(params[:username])
+  api_response(@error, "Unfollowed #{params[:username]}")
 end
 
 # Start the Sinatra application
