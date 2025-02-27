6a59c06d67ab8eb5991d3b75f7a0eb60227ab2e6
Nikolaj <nals@itu.dk>
2025-02-27 21:03:48 +0100

    Delete run_tests.sh
    Removed redundant process

--- Test/run_tests.sh
+++ Test/run_tests.sh
@@ -9,6 +9,3 @@ sleep 5
 # Run the tests
 rspec Test/minitwit_tests_spec.rb || echo "RSpec tests failed!"
 pytest Test/minitwit_sim_api_test.py || echo "Python tests failed!"
-
-# Optionally kill the app after tests complete
-# pkill -f "ruby minitwit.rb"
