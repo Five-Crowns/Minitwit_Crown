07e2294e40327f97c35b171f600347bced00249e
sths <stinehelena09@gmail.com>
2025-04-01 12:33:13 +0200

    Added JSON logging in logging.rb

--- logger.rb
+++ logger.rb
@@ -2,10 +2,23 @@
 
 require 'logger'
 
+class JSONLogger < Logger
+  def format_message(severity, timestamp, progname, msg)
+    log_entry = {
+      timestamp: timestamp,
+      level: severity,
+      message: msg
+    }
+
+    #This converts to JSON for elasticSearch
+    "#{log_entry[:timestamp]}:#{log_entry[:level]}: #{msg}\n".to_json
+  end
+end
+
 LOG_FILE = File.expand_path('log/minitwit.log', __dir__)
 
 class MinitwitLogger
   def self.logger
-    @logger ||= Logger.new(LOG_FILE, 'daily')
+    @logger ||= JSONLogger.new(LOG_FILE, 'daily')
   end
 end
