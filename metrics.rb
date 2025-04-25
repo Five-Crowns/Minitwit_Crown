6f3cd5f86fe18f3a3c64ae4a748e94462cd5e7ec
Bryce Karnikar <brka@itu.dk>
2025-04-25 15:15:09 +0200

    Applied standardrb fix

--- metrics.rb
+++ metrics.rb
@@ -1,4 +1,4 @@
-require 'prometheus/client'
+require "prometheus/client"
 
 module Metrics
   module_function
@@ -8,7 +8,7 @@ module Metrics
   def self.http_requests_total
     @http_requests_total ||= Prometheus::Client::Counter.new(
       :http_requests_total,
-      docstring: 'Total number of HTTP requests received',
+      docstring: "Total number of HTTP requests received",
       labels: %i[method route status]
     )
   end
@@ -16,7 +16,7 @@ module Metrics
   def self.http_request_duration_seconds
     @http_request_duration_seconds ||= Prometheus::Client::Histogram.new(
       :http_request_duration_seconds,
-      docstring: 'Histogram for tracking request duration',
+      docstring: "Histogram for tracking request duration",
       labels: %i[method route status]
     )
   end
@@ -24,14 +24,14 @@ module Metrics
   def self.active_users
     @active_users ||= Prometheus::Client::Gauge.new(
       :active_users,
-      docstring: 'Currently active users'
+      docstring: "Currently active users"
     )
   end
 
   def self.db_create_msg_duration
     @db_create_msg_duration ||= Prometheus::Client::Histogram.new(
       :db_create_msg_duration,
-      docstring: 'Database query time for creating a new message',
+      docstring: "Database query time for creating a new message",
       labels: [:endpoint]
     )
   end
@@ -39,7 +39,7 @@ module Metrics
   def self.db_get_msgs_duration
     @db_get_msgs_duration ||= Prometheus::Client::Histogram.new(
       :db_get_msgs_duration,
-      docstring: 'Database query time for get_messages',
+      docstring: "Database query time for get_messages",
       labels: [:endpoint]
     )
   end
@@ -47,7 +47,7 @@ module Metrics
   def self.db_get_msgs_by_user_duration
     @db_get_msgs_by_user_duration ||= Prometheus::Client::Histogram.new(
       :db_get_msgs_by_user_duration,
-      docstring: 'Database query time for get_messages for single user',
+      docstring: "Database query time for get_messages for single user",
       labels: [:endpoint]
     )
   end
@@ -55,7 +55,7 @@ module Metrics
   def self.db_get_followers_by_user_duration
     @db_get__by_followersuser_duration ||= Prometheus::Client::Histogram.new(
       :db_get_followers_by_user_duration,
-      docstring: 'Database query time for get_followers for single user',
+      docstring: "Database query time for get_followers for single user",
       labels: [:endpoint]
     )
   end
@@ -63,7 +63,7 @@ module Metrics
   def self.db_follow_user_duration
     @db_follow_user_duration ||= Prometheus::Client::Histogram.new(
       :db_follow_user_duration,
-      docstring: 'Database query time for following a user',
+      docstring: "Database query time for following a user",
       labels: [:endpoint]
     )
   end
@@ -71,7 +71,7 @@ module Metrics
   def self.db_unfollow_user_duration
     @db_unfollow_user_duration ||= Prometheus::Client::Histogram.new(
       :db_unfollow_user_duration,
-      docstring: 'Database query time for unfollowing a user',
+      docstring: "Database query time for unfollowing a user",
       labels: [:endpoint]
     )
   end
@@ -79,7 +79,7 @@ module Metrics
   def self.db_register_user_duration
     @db_register_user_duration ||= Prometheus::Client::Histogram.new(
       :db_register_user_duration,
-      docstring: 'Database query time for registering a new user',
+      docstring: "Database query time for registering a new user",
       labels: [:endpoint]
     )
   end
