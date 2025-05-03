f04c8a9a9549912b51c6b4699916aa4b0ec35d22
Nikolaj <nals@itu.dk>
2025-05-03 12:12:15 +0200

    Update 20250503120534_add_indexes_to_messages.rb

--- db/migrate/20250503120534_add_indexes_to_messages.rb
+++ db/migrate/20250503120534_add_indexes_to_messages.rb
@@ -1,8 +1,8 @@
 class AddIndexesToMessages < ActiveRecord::Migration[6.1]
-    def change
-      add_index :messages, :flagged
-      add_index :messages, :author_id
-      add_index :messages, :pub_date
-      add_index :messages, [:flagged, :pub_date]
-    end
+  def change
+    add_index :messages, :flagged
+    add_index :messages, :author_id
+    add_index :messages, :pub_date
+    add_index :messages, [:flagged, :pub_date]
   end
+end
